5feb8db3031cb7cb96f90676dc31a88128e275b1
Deepak Shah <deepak@infosofttech.com>
2024-07-29 21:27:18 +0530

    Added more products

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,7 +24,10 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  
   return (
+
+    
     <html lang="en">
       <body className={inter.className}>
         <HeaderTop />
