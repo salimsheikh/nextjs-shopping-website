21b26088cffaada9052f420cdaecd749c9cbd615
Deepak Shah <deepak@infosofttech.com>
2024-07-29 17:58:55 +0530

    Banner Completed

--- src/components/HomeSlider.tsx
+++ src/components/HomeSlider.tsx
@@ -20,21 +20,21 @@ const HomeSlider = () => {
           img: "/banner-1.jpg",
           title: "Trending Item",
           mainTitle: "WOMEN'S LATEST FASHION SALE",
-          price: "$20"
+          price: "$20.00"
         },
         {
           id:2,
           img: "/banner-2.jpg",
           title: "Trending Accessories",
           mainTitle: "MODERN SUNGLASSES",
-          price: "$15"
+          price: "$15.00"
         },
         {
           id:3,
           img: "/banner-3.jpg",
           title: "Scale Offer",
           mainTitle: "NEW FASHION SUMMER SALE",
-          price: "$30"
+          price: "$30.00"
         }
       ];
   return (
